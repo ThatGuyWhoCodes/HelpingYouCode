2a6a6c1fccc19d32ffb0259749c219022164bcfc
ThatCodeGuy <colegood2008@gmail.com>
2026-03-23 09:40:58 +0000

    Update model.py

--- model.py
+++ model.py
@@ -1,5 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin
+from datetime import datetime
 
 db = SQLAlchemy()
 
@@ -7,6 +8,14 @@ class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(100), unique=True)
     password = db.Column(db.String(100))
+    role = db.Column(db.String(20))  # customer / producer
+    loyalty_points = db.Column(db.Integer, default=0)
+
+class ProducerProfile(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    story = db.Column(db.Text)
+    methods = db.Column(db.Text)
 
 class Product(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -14,3 +23,17 @@ class Product(db.Model):
     price = db.Column(db.Float)
     stock = db.Column(db.Integer)
     owner_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+class Order(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer)
+    total_price = db.Column(db.Float)
+    delivery_type = db.Column(db.String(20))  # delivery/collection
+    scheduled_time = db.Column(db.String(50))
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+
+class OrderItem(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    order_id = db.Column(db.Integer)
+    product_id = db.Column(db.Integer)
+    quantity = db.Column(db.Integer)
